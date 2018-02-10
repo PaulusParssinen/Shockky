339dd339d8ddc7e21608efd2cd1be5191ecb5a63
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-02-10 16:18:08 +0200

    Started the implementation of ShockwaveWriter

---
+++ Shockky/IO/ShockwaveWriter.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using Shockky.Shockwave;
+
+namespace Shockky.IO
+{
+    public class ShockwaveWriter : BinaryWriter //TODO:
+    {
+        private readonly bool _leaveOpen;
+
+        public long Position
+        {
+            get => BaseStream.Position;
+            set => BaseStream.Position = value;
+        }
+        public long Length => BaseStream.Length;
+
+        protected int BitPosition { get; set; }
+        protected int BitContainer { get; set; }
+
+        public ShockwaveWriter()
+            : this(0)
+        { }
+        public ShockwaveWriter(byte[] data)
+            : this(data.Length)
+        {
+            Write(data, 0, data.Length);
+        }
+        public ShockwaveWriter(int capacity)
+            : this(new MemoryStream(capacity))
+        { }
+
+        public ShockwaveWriter(Stream output)
+            : this(output, new ASCIIEncoding(), false)
+        { }
+        public ShockwaveWriter(Stream output, bool leaveOpen)
+            : this(output, new ASCIIEncoding(), leaveOpen)
+        { }
+        public ShockwaveWriter(Stream output, Encoding encoding)
+            : this(output, encoding, false)
+        { }
+        public ShockwaveWriter(Stream output, Encoding encoding, bool leaveOpen)
+            : base(output, encoding)
+        {
+            _leaveOpen = leaveOpen;
+        }
+
+        public void Write(ShockwaveItem item)
+        {
+            item.WriteTo(this);
+        }
+    }
+}
