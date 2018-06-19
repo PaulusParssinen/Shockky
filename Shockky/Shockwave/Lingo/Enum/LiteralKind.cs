f05ccc74a234c75962e4b197e1788819a474613b
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:10:32 +0300

    Beautified

---
+++ Shockky/Shockwave/Lingo/Enum/LiteralKind.cs
@@ -0,0 +1,9 @@
+﻿namespace Shockky.Shockwave.Lingo.Enum
+{
+    public enum LiteralKind
+    {
+        String = 1,
+        Integer = 4,
+        Float = 9
+    }
+}
---
+++ Shockky/Shockwave/Lingo/Literal/LingoLiteral.cs
@@ -0,0 +1,57 @@
+﻿using Shockky.IO;
+using Shockky.Shockwave.Lingo.Enum;
+
+namespace Shockky.Shockwave.Lingo
+{
+    public class LingoLiteral : ShockwaveItem
+    {
+        protected override string DebuggerDisplay => $"{Kind} | Value: {Value ?? "NULL"}";
+
+        public LiteralKind Kind { get; set; }
+        public int Offset { get; set; }
+        public object Value { get; set; }
+
+        public LingoLiteral(LiteralKind kind, object value)
+        {
+            Kind = kind;
+            Value = value;
+        }
+
+        public LingoLiteral(ShockwaveReader input)
+        {
+            Kind = (LiteralKind)input.ReadBigEndian<int>();
+            Offset = input.ReadBigEndian<int>();
+        }
+
+        public void ReadValue(int dataOffset, ShockwaveReader input)
+        {
+            if (Kind != LiteralKind.Integer) 
+            {
+                input.Position = dataOffset + Offset;
+
+                int length = input.ReadBigEndian<int>();
+
+                if (Kind == LiteralKind.String)
+                    Value = input.ReadString(length - 1); //Might actually also be null byte delimeter str?
+                else Value = input.ReadBigEndian<long>();
+            }
+            else Value = Offset;
+        }
+
+        //TODO: Could create static Create method
+
+        public override int GetBodySize()
+        {
+            int size = 0;
+            size += sizeof(int);
+            size += sizeof(int);
+            return size;
+        }
+
+        public override void WriteTo(ShockwaveWriter output)
+        {
+            output.WriteBigEndian((int)Kind);
+            output.WriteBigEndian(Offset);
+        }
+    }
+}
