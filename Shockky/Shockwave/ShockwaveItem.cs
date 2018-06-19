e038b3787f116dd17050f5a8ff21b7a7f6ec2c43
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 20:47:45 +0300

    DebuggingDisplay for shockwave items to make life easier

--- Shockky/Shockwave/ShockwaveItem.cs
+++ Shockky/Shockwave/ShockwaveItem.cs
@@ -1,10 +1,15 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using Shockky.IO;
 
 namespace Shockky.Shockwave
 {
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public abstract class ShockwaveItem
     {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        protected virtual string DebuggerDisplay => "{" + ToString() + "}";
+
         public byte[] ToArray()
         {
             using (var outputMem = new MemoryStream())
