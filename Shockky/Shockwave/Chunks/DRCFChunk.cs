f6ea86368ada1e8d662b68cc33a31f1bf01312d3
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:23:17 +0300

    Oh god

--- Shockky/Shockwave/Chunks/DRCFChunk.cs
+++ Shockky/Shockwave/Chunks/DRCFChunk.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
 using Shockky.IO;
 
 namespace Shockky.Shockwave.Chunks
@@ -7,17 +9,72 @@ namespace Shockky.Shockwave.Chunks
     {
         public string VersionHex { get; set; }
 
-        public DRCFChunk(ShockwaveReader input, ChunkEntry entry)
-            : base(entry.Header)
+        //What a beauty
+        public DRCFChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
         {
-            input.Position += 36; //yeah no idea what data is in there
-            var versionBytes = input.ReadBytes(2);
-            
-            VersionHex = BitConverter.ToString(versionBytes)
-                .Replace("-", string.Empty);;
+            Debug.Assert(header.Length == 100, "HMMMM");
+
+            short length = input.ReadBigEndian<short>();
+            ushort version = input.ReadBigEndian<ushort>();
+            VersionHex = version.ToString("X");
+
+            var rect = input.ReadRect(true);
+
+            short minMember = input.ReadBigEndian<short>(); //and max are Obsolete -Docs
+            short maxMember = input.ReadBigEndian<short>();
+
+            byte tempo = input.ReadByte();
+            byte unk1 = input.ReadByte();
+
+            byte g = input.ReadByte();
+            byte b = input.ReadByte();
+
+            short unk2 = input.ReadBigEndian<short>();
+            short unk3 = input.ReadBigEndian<short>();
+
+            short unk4 = input.ReadBigEndian<short>();
+
+            //ripping from docs
+            byte stagePropertiesWhathteufkcdsigfhsdfg = input.ReadByte();
+
+            var stageColorThingy = Color.FromArgb(255, stagePropertiesWhathteufkcdsigfhsdfg, g, b);
+
+            byte unk5 = input.ReadByte();
+            short unk6 = input.ReadBigEndian<short>();
+            byte unk7 = input.ReadByte(); //-2 when version < 0x551
+            byte unk8 = input.ReadByte();
+            int unk9 = input.ReadBigEndian<int>();
+
+            short movieVersion = input.ReadBigEndian<short>();
+            short unk10 = input.ReadBigEndian<short>();//16?
+            int unk11 = input.ReadBigEndian<int>();
+            int unk12 = input.ReadBigEndian<int>();
+            int unk13 = input.ReadBigEndian<int>();
+            byte unk14 = input.ReadByte();
+            byte unk15 = input.ReadByte();
+            short unk16 = input.ReadBigEndian<short>();
+            short unk17 = input.ReadBigEndian<short>();
+            short unk18 = input.ReadBigEndian<short>();
+            int unk19 = input.ReadBigEndian<int>();
+            int unk20 = input.ReadBigEndian<int>();
+            short oldDefaultPalette = input.ReadBigEndian<short>();
+            short unk21 = input.ReadBigEndian<short>();
+            int unk22 = input.ReadBigEndian<int>();
+            int defaultPalette = input.ReadBigEndian<int>(); //TODO RGB?
+            short unk23 = input.ReadBigEndian<short>(); //two bytes
+            short unk24 = input.ReadBigEndian<short>();
+            int downloadFramesBeforePlaying = input.ReadBigEndian<int>(); //90
+            //Zeros
+            short unk25 = input.ReadBigEndian<short>();
+            short unk26 = input.ReadBigEndian<short>();
+            short unk27 = input.ReadBigEndian<short>();
+            short unk28 = input.ReadBigEndian<short>();
+            short unk29 = input.ReadBigEndian<short>();
+            short unk30 = input.ReadBigEndian<short>();
         }
 
-        public override void WriteTo(ShockwaveWriter output)
+        public override void WriteBodyTo(ShockwaveWriter output)
         {
             throw new NotImplementedException();
         }
@@ -25,6 +82,7 @@ namespace Shockky.Shockwave.Chunks
         public override int GetBodySize()
         {
             int size = 0;
+            
             size += 36;
             size += sizeof(byte) * 2;
             return size;
---
+++ Shockky/Shockwave/Chunks/RequiredComponentLinkageChunk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shockky.IO;
+
+namespace Shockky.Shockwave.Chunks
+{
+    public class RequiredComponentLinkageChunk : ChunkItem
+    {
+        public RequiredComponentLinkageChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
+        {
+            int unk0 = input.ReadBigEndian<int>(); //unk for a reason
+            int count = input.ReadBigEndian<int>(); 
+            for (int i = 0; i < count; i++)
+            {
+                int count1 = input.ReadBigEndian<int>();
+                int wtf = input.ReadBigEndian<int>();
+                int realGuidLengthIthink = input.ReadBigEndian<int>();
+                byte[] componentGUID = input.ReadBytes(realGuidLengthIthink);
+                for (int j = 0; j < count1; j++)
+                {
+                    short unk2 = input.ReadBigEndian<short>();
+                    string componentName = input.ReadString();
+                    byte nullByte = input.ReadByte();
+                }
+            }
+        }
+
+        public override int GetBodySize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteBodyTo(ShockwaveWriter output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
