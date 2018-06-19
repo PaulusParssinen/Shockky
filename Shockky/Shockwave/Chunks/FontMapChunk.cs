d4a68a82a3bcba8ad94df066182e909e02cc9d43
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:25:16 +0300

    Added FontMap and SortOrder chunks.

---
+++ Shockky/Shockwave/Chunks/FontMapChunk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Shockky.IO;
+
+namespace Shockky.Shockwave.Chunks
+{
+    public class FontMapChunk : ChunkItem
+    {
+        public FontMapChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
+        {
+            string fontmap = input.ReadString((int)header.Length);
+        }
+
+        public override int GetBodySize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteBodyTo(ShockwaveWriter output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shockky/Shockwave/Chunks/SortOrderChunk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Shockky.IO;
+
+namespace Shockky.Shockwave.Chunks
+{
+    public class SortOrderChunk : ChunkItem
+    {
+        public List<int> Entries { get; set; }
+
+        public SortOrderChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
+        {
+            int unk0 = input.ReadBigEndian<int>();
+            int unk1 = input.ReadBigEndian<int>();
+
+            Entries = new List<int>(input.ReadBigEndian<int>());
+
+            int unk2 = input.ReadBigEndian<int>();
+            int unk3 = input.ReadBigEndian<int>();
+
+            for (int i = 0; i < Entries.Capacity; i++)
+            {
+                Entries.Add(input.ReadBigEndian<int>());
+            }
+        }
+
+        public override int GetBodySize()
+        {
+            int size = 0;
+            size += sizeof(int);
+            size += sizeof(int);
+            size += sizeof(int);
+            size += sizeof(int);
+            size += sizeof(int);
+            size += sizeof(int) * Entries.Count;
+            return size;
+        }
+
+        public override void WriteBodyTo(ShockwaveWriter output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
