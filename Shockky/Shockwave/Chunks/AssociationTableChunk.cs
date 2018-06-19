ef86a7b6844916da6ef31ac272cdb8ca2c97efe0
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:22:33 +0300

    Got more info on association table chunk.

--- Shockky/Shockwave/Chunks/AssociationTableChunk.cs
+++ Shockky/Shockwave/Chunks/AssociationTableChunk.cs
@@ -1,38 +1,40 @@
 ﻿using Shockky.IO;
 using Shockky.Shockwave.Chunks.Cast;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Shockky.Shockwave.Chunks
 {
     public class AssociationTableChunk : ChunkItem
     {
-        public short Unknown1 { get; }
-        public short Unknown2 { get; }
-
-        public int Unknown3 { get; set; }
+        public short EntrySize { get; }
+        public short Unknown1 { get; set; }
+        public int TotalCount { get; set; }
+        public int AssignedCount { get; set; }
 
         public List<CastEntry> CastEntries { get; set; }
 
-        public AssociationTableChunk(ShockwaveReader input, ChunkEntry entry)
-            : base(entry.Header)
+        public AssociationTableChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
         {
+            EntrySize = input.ReadInt16(); //entry size, constant i think
             Unknown1 = input.ReadInt16();
-            Unknown2 = input.ReadInt16();
-            Unknown3 = input.ReadInt32();
-
-            CastEntries = new List<CastEntry>(input.ReadInt32());
+            TotalCount = input.ReadInt32();
+            AssignedCount = input.ReadInt32();
+            CastEntries = new List<CastEntry>(TotalCount);
             for (int i = 0; i < CastEntries.Capacity; i++)
             {
                 CastEntries.Add(new CastEntry(input));
             }
+
+            //TODO
+            CastEntries.RemoveRange(AssignedCount, TotalCount - AssignedCount);
         }
 
-        public override void WriteTo(ShockwaveWriter output)
+        public override void WriteBodyTo(ShockwaveWriter output)
         {
+            output.Write(EntrySize);
             output.Write(Unknown1);
-            output.Write(Unknown2);
-            output.Write(Unknown3);
+            output.Write(TotalCount);
             output.Write(CastEntries.Count); //TODO: nil check
             foreach (var entry in CastEntries)
             {
@@ -47,7 +49,7 @@ namespace Shockky.Shockwave.Chunks
             size += sizeof(short);
             size += sizeof(int);
             size += sizeof(int);
-            size += CastEntries.Sum(ce => ce.GetBodySize());
+            size += EntrySize * TotalCount;
             return size;
         }
     }
