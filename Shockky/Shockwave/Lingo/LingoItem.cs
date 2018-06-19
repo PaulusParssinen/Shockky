48487697e50ac27081cf6e6cb3562e3ab5dd7a5d
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:15:15 +0300

    Added base class for the classes that can be turned to Lingo code

---
+++ Shockky/Shockwave/Lingo/LingoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Shockky.Shockwave.Chunks;
+
+namespace Shockky.Shockwave.Lingo
+{
+    public abstract class LingoItem : ShockwaveItem
+    {
+        protected ScriptChunk Script { get; }
+
+        protected LingoItem(ScriptChunk script)
+        {
+            Script = script;
+        }
+
+        public ScriptChunk GetScript() => Script;
+
+        public virtual string ToLingo()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
