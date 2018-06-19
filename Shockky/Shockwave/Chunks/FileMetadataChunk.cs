449fb95a813407df23ff75f20cfcff88261d2e95
PaulusParssinen <paulus.parssinen01@edupori.fi>
2018-06-19 21:24:11 +0300

    Added chunk to read metadata of the file (RIFX)

---
+++ Shockky/Shockwave/Chunks/FileMetadataChunk.cs
@@ -0,0 +1,34 @@
+﻿using Shockky.IO;
+using Shockky.Shockwave.Chunks.Enum;
+
+namespace Shockky.Shockwave.Chunks
+{
+    public class FileMetadataChunk : ChunkItem
+    {
+        public long FileLength => Header.Length;
+
+        public CodecKind Codec { get; set; }
+
+        public FileMetadataChunk(ShockwaveReader input)
+            : base(new ChunkHeader(input))
+        {
+            Codec = input.ReadReversedString(4).ToCodec();
+        }
+        public FileMetadataChunk(ShockwaveReader input, ChunkHeader header)
+            : base(header)
+        {
+            Codec = input.ReadReversedString(4).ToCodec();
+        }
+
+        public override int GetBodySize()
+        {
+            int size = 0;
+            size += 4;
+            return size;
+        }
+        public override void WriteBodyTo(ShockwaveWriter output)
+        {
+            output.WriteBigEndian(Codec);
+        }
+    }
+}
