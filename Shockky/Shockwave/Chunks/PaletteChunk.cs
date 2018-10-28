aa4ed41a124e418293d22f30cee625747af9206d
paulusparssinen <paulus.parssinen01@edupori.fi>
2018-10-29 01:25:38 +0200

    Colors

--- Shockky/Shockwave/Chunks/PaletteChunk.cs
+++ Shockky/Shockwave/Chunks/PaletteChunk.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Drawing;
+using System;
 
 using Shockky.IO;
 
@@ -6,14 +7,18 @@ namespace Shockky.Shockwave.Chunks
 {
     public class PaletteChunk : ChunkItem
     {
+        public Color[] Colors { get; }
+
         public PaletteChunk(ShockwaveReader input, ChunkHeader header)
             : base(header)
         {
-            for(int i = 0; i < (header.Length / 6); i++)
+            Colors = new Color[header.Length / 6];
+            for (int i = 0; i < Colors.Length; i++)
             {
                 int r = input.ReadInt16();
                 int g = input.ReadInt16();
                 int b = input.ReadInt16();
+                Colors[i] = Color.FromArgb(r, g, b);
             }
         }
 
@@ -24,7 +29,9 @@ namespace Shockky.Shockwave.Chunks
 
         public override int GetBodySize()
         {
-            throw new NotImplementedException();
+            int size = 0;
+            size += (Colors.Length * 6);
+            return size;
         }
     }
 }
