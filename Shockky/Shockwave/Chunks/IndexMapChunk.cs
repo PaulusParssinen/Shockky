21ca89ba77e12e8a52b4cd83b396f4df4ff55534
paulusparssinen <paulus.parssinen01@edupori.fi>
2018-10-30 21:26:09 +0200

    Imma change that

--- Shockky/Shockwave/Chunks/IndexMapChunk.cs
+++ Shockky/Shockwave/Chunks/IndexMapChunk.cs
@@ -1,19 +1,18 @@
-﻿using System.Collections.Generic;
-using Shockky.IO;
+﻿using Shockky.IO;
 
 namespace Shockky.Shockwave.Chunks
 {
     public class IndexMapChunk : ChunkItem
     {
-        public List<int> MemoryMapOffsets { get; }
+        public int[] MemoryMapOffsets { get; }
 
         public IndexMapChunk(ShockwaveReader input, ChunkHeader header)
             : base(header)
         {
-            MemoryMapOffsets = new List<int>(input.ReadInt32());
-            for (int i = 0; i < MemoryMapOffsets.Capacity; i++)
+            MemoryMapOffsets = new int[input.ReadInt32()];
+            for (int i = 0; i < MemoryMapOffsets.Length; i++)
             {
-                MemoryMapOffsets.Add(input.ReadInt32());
+                MemoryMapOffsets[i] = input.ReadInt32();
             }
         }
 
@@ -21,14 +20,14 @@ namespace Shockky.Shockwave.Chunks
         {
             int size = 0;
             size += sizeof(int);
-            size += sizeof(int) * MemoryMapOffsets.Count;
+            size += sizeof(int) * MemoryMapOffsets.Length;
             return size;
         }
 
         public override void WriteBodyTo(ShockwaveWriter output)
         {
-            output.Write(MemoryMapOffsets.Count);
-            for (int i = 0; i < MemoryMapOffsets.Count; i++)
+            output.Write(MemoryMapOffsets.Length);
+            for (int i = 0; i < MemoryMapOffsets.Length; i++)
             {
                 output.Write(MemoryMapOffsets[i]);
             }
