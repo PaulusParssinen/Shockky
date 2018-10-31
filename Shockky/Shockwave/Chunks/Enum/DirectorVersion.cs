e62a8c3c6b098847147a3863baaff59674308f0e
paulusparssinen <paulus.parssinen01@edupori.fi>
2018-10-31 20:18:12 +0200

    Seems like it's that way instead

---
+++ Shockky/Shockwave/Chunks/Enum/DirectorVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace Shockky.Shockwave.Chunks
+{
+    public enum DirectorVersion
+    {
+        V4 = 0,
+        V5 = 0x4c1,
+        V6 = 0x4c7,
+        V85 = 0x708,
+        V10 = 0x742,
+        VHMM = 0x782
+    }
+}
