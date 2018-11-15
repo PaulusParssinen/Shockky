28a035bab5ad235881e084075b615c34ca8450fa
paulusparssinen <paulus.parssinen01@edupori.fi>
2018-11-15 20:04:03 +0200

    Let's add those for now. Gotta think of something smart in order to handle the cast member media chunks.

---
+++ Shockky/Shockwave/Chunks/BitmapChunk.cs
@@ -0,0 +1,11 @@
+﻿using Shockky.IO;
+
+namespace Shockky.Shockwave.Chunks
+{
+    public class BitmapChunk : BinaryDataChunk
+    {
+        public BitmapChunk(ShockwaveReader input, ChunkHeader header)
+            : base(input, header)
+        { }
+    }
+}
