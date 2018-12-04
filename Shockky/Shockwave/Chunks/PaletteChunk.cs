a8d4496878014ab6396d2feb740391be9d3920fc
paulusparssinen <paulus.parssinen01@edupori.fi>
2018-12-04 20:26:19 +0200

    Fucking hell, might be broken for some file versions

--- Shockky/Shockwave/Chunks/PaletteChunk.cs
+++ Shockky/Shockwave/Chunks/PaletteChunk.cs
@@ -11,19 +11,27 @@ namespace Shockky.Shockwave.Chunks
         public PaletteChunk(ShockwaveReader input, ChunkHeader header)
             : base(header)
         {
-            Colors = new Color[(header.Length / 6)];
+            Colors = new Color[header.Length / 6];
             for (int i = 0; i < Colors.Length; i++)
             {
-                int r = input.ReadInt16() & 0x7F;
-                int g = input.ReadInt16() & 0x7F;
-                int b = input.ReadInt16() & 0x7F;
+                //Oh god help me
+                byte[] r = input.ReadBytes(2);
+                byte[] g = input.ReadBytes(2);
+                byte[] b = input.ReadBytes(2);
 
-                Colors[i] = Color.FromArgb(r, g, b);
+                /*
+                int r = input.ReadBigEndian<ushort>() & 0x7F;
+                int g = input.ReadBigEndian<ushort>() & 0x7F;
+                int b = input.ReadBigEndian<ushort>() & 0x7F;
+                */
+                Colors[i] = Color.FromArgb(r[0], g[0], b[0]);
             }
         }
 
         public override void WriteBodyTo(ShockwaveWriter output)
         {
+            throw new System.NotImplementedException();
+
             foreach(var color in Colors)
             {
                 output.Write((short)color.R);
