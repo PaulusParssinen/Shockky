e242cf7e12c30a89f0430c8d1cd2e5fb7395347e
PaulusParssinen <paulus.parssinen01@edupori.fi>
2019-02-19 12:32:31 +0200

    Added bunch of Director versions

--- Shockky/Shockwave/Chunks/Enum/DirectorVersion.cs
+++ Shockky/Shockwave/Chunks/Enum/DirectorVersion.cs
@@ -3,10 +3,18 @@
     public enum DirectorVersion
     {
         V4 = 0,
-        V5 = 0x4c1,
-        V6 = 0x4c7,
-        V85 = 0x708,
+        V5 = 0x4C1,
+        V6 = 0x4C7,
+        V7 = 0x57E,
+        V8 = 0x640,
+        V8_5 = 0x708,
+        V9 = 0x73A, //Could also be 8.5 
         V10 = 0x742,
-        VHMM = 0x782
+        V10_1 = 0x744,
+        V11_1 = 0x782,
+        V11_1_5 = 0x783,
+        V12 = 0x79F,
+
+        Max = 0x163C
     }
 }
