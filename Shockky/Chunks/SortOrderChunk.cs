d3ed3547bac692a86ea479ac55672bf91b8a42e0
PaulusParssinen <paulus.parssinen01@edupori.fi>
2020-05-16 23:58:35 +0300

    Fix some SortOrderChunk values

--- Shockky/Chunks/SortOrderChunk.cs
+++ Shockky/Chunks/SortOrderChunk.cs
@@ -14,13 +14,14 @@ namespace Shockky.Chunks
         public SortOrderChunk(ref ShockwaveReader input, ChunkHeader header)
             : base(header)
         {
-            Remnants.Enqueue(input.ReadInt32());
-            Remnants.Enqueue(input.ReadInt32());
+            Remnants.Enqueue(input.ReadInt32()); //TODO: I think I'm seeing a pattern here 🤔 See: ScriptContextChunk
+            Remnants.Enqueue(input.ReadInt32()); //0 0, count, count, offset, entry length
 
             Entries = new List<int>(input.ReadInt32());
-
-            Remnants.Enqueue(input.ReadInt32());
-            Remnants.Enqueue(input.ReadInt32());
+            input.ReadInt32();
+            
+            input.ReadInt16();
+            input.ReadInt16();
 
             for (int i = 0; i < Entries.Capacity; i++)
             {
@@ -36,8 +37,8 @@ namespace Shockky.Chunks
 
             size += sizeof(int);
 
-            size += sizeof(int);
-            size += sizeof(int);
+            size += sizeof(short);
+            size += sizeof(short);
 
             size += sizeof(int) * Entries.Count;
             return size;
@@ -45,13 +46,17 @@ namespace Shockky.Chunks
 
         public override void WriteBodyTo(ShockwaveWriter output)
         {
+            const short ENTRIES_OFFSET = 20;
+            const short ENTRY_SIZE = sizeof(int);
+
             output.Write((int)Remnants.Dequeue());
             output.Write((int)Remnants.Dequeue());
 
             output.Write(Entries.Count);
+            output.Write(Entries.Count);
 
-            output.Write((int)Remnants.Dequeue());
-            output.Write((int)Remnants.Dequeue());
+            output.Write(ENTRIES_OFFSET);
+            output.Write(ENTRY_SIZE);
 
             for (int i = 0; i < Entries.Count; i++)
             {
